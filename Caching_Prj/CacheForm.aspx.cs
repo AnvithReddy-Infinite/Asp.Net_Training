3a8a03884a62731ff6587ef351555d60f645f06a
AnvithReddy-Infinite <anvithr@infinite.com>
2025-12-31 13:26:06 +0530

    Updating & adding files

---
+++ Caching_Prj/CacheForm.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.UI;
+
+namespace Caching_Prj
+{
+    public partial class CacheForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetProductsByName("All");
+
+                // Caching
+                Response.Cache.SetExpires(DateTime.Now.AddSeconds(30));
+                Response.Cache.SetCacheability(HttpCacheability.ServerAndPrivate);
+                Response.Cache.VaryByParams["None"] = true;
+            }
+
+            lblmsg.Text = "This Page is cached at : " + DateTime.Now.ToString();
+        }
+
+        private void GetProductsByName(string productname)
+        {
+            using (SqlConnection con = new SqlConnection(
+                "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Infinite;Integrated Security=True"))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter("spGetProductByName", con))
+                {
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    da.SelectCommand.Parameters.AddWithValue("@Productname", productname);
+
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+
+                    GridView1.DataSource = ds;
+                    GridView1.DataBind();
+                }
+            }
+        }
+
+        protected void DDL_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetProductsByName(DDL.SelectedValue);
+        }
+    }
+}
---
+++ Security_Default_Prj/WebForm3.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm3.aspx.cs" Inherits="Security_Default_Prj.WebForm3" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+<asp:Label ID="lbl1" runat="server"></asp:Label>
+
+</asp:Content>
